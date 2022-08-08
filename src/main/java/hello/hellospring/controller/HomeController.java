0d8bb164cdabb86f1922aca34b1e160ba08da005
LimYeongJin <ant2725@naver.com>
2022-08-08 23:24:33 +0900

    회원 웹 기능 수강

---
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // localhost:8080 으로 들어오면 이게 호출
+    // url에 오는 것이 아무것도 없으면 정적 컨텐츠 찾아야 하지 않냐? => 정적컨텐츠 이미지.PNG 참조
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 스프링이 처음에 실행될 때 스프링 컨테이너라는 통이 생기는데 거기에 @Controller가 있으면
 //  MemberController 객체를 생성해 스프링 컨테이너 안에 넣는다.
@@ -22,5 +28,32 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // GET : URL에 직접 치는 방식(주로 조회할 때 사용)
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // POST : 데이터를 form 같은 곳에 넣어 전달(members/createMemberForm.html 참조)
+    // MemberForm 타입을 파라미터로 넣었는데 이 안의 name 변수에 Spring이 form에 있는 name과 대응시킨다.
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
