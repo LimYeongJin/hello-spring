88c0ea1b63b93ccae27a6c619eafe08733e8de38
LimYeongJin <ant2725@naver.com>
2022-08-19 00:02:45 +0900

    스프링 강의(스프링 데이터 JPA)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,21 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 // 자바 코드로 스프링 빈 등록
 // @Configuration 클래스에 @Bean 메서드(생성자) 등록
 @Configuration
 public class SpringConfig {
 
+    // 스프링 데이터 JPA 관련
+    private final MemberRepository memberRepository;
 
+    // 생성자 하나인 경우는 @Autowired 생략 가능
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+    /*
+    // 스프링 데이터 JPA 사용해야 해서 주석 처리
+
+    // JPA 관련
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+     */
 
     /*
     // JPA 사용해야 해서 주석 처리
@@ -31,29 +45,35 @@ public class SpringConfig {
     }
      */
 
+    /*
+    // 스프링 데이터 JPA 사용해야 해서 주석 처리
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+     */
 
+    /*
+    // 스프링 데이터 JPA 사용해야 해서 주석 처리
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
-        /*
-        스프링이 좋은 이유 : 다형성 활용 좋음
-        인터페이스 구현체를 편리하게 바꿔 끼기 할 수 있다.
-        개방 폐쇄 원칙(OCP, Open-Closed Principle) : 확장에는 열려있고, 수정, 변경에는 닫혀있음
-         */
+
+        // 스프링이 좋은 이유 : 다형성 활용 좋음
+        // 인터페이스 구현체를 편리하게 바꿔 끼기 할 수 있다.
+        // 개방 폐쇄 원칙(OCP, Open-Closed Principle) : 확장에는 열려있고, 수정, 변경에는 닫혀있음
+
         //return new JdbcMemberRepository(dataSource);
-        /*
-        JdbcTemplate 관련 리포지토리 활용
+
+        // JdbcTemplate 관련 리포지토리 활용
         리포지토리 만들고 테스트하고 싶을 때, SpringConfig로 관리하면 여기만 바꿔주면 빠르게 테스트 가능
-         */
+
         // return new JdbcTemplateMemberRepository(dataSource);
-        /*
-        JPA 사용
-         */
+
+        // JPA 사용
+
         return new JpaMemberRepository(em);
     }
+    */
 
 }
