b843afe5be8e53d3835c7db299e7dde6491ca72d
LimYeongJin <ant2725@naver.com>
2022-08-24 01:23:40 +0900

    전체 복습

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-// 스프링 데이터 JPA가 JpaRepository를 상속하고 있으면 구현체를 자동으로 생성
-// 따라서 스프링 데이터 JPA Spring Bean을 자동으로 등록
-// SpringConfig 참조
+/**
+ * 스프링 데이터 JPA가 JpaRepository를 상속하고 있으면 구현체를 자동으로 생성
+ * 따라서 스프링 데이터 JPA Spring Bean을 자동으로 등록
+ * SpringConfig 참조
+ */
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
     // JPQL select m from Member m where m.name = ?
