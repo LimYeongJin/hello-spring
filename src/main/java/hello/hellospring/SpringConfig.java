c94b130450e8a127c5e03b177696dd96da4bf67f
LimYeongJin <ant2725@naver.com>
2022-10-03 23:58:24 +0900

    공부

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,9 +6,10 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-// 자바 코드로 스프링 빈 등록
-// @Configuration 클래스에 @Bean 메서드(생성자) 등록
+/**
+    자바 코드로 스프링 빈 등록
+    @Configuration 클래스에 @Bean 메서드(생성자) 등록
+ */
 @Configuration
 public class SpringConfig {
 
